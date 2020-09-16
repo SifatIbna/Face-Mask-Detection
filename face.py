090dbfa49c2d814c3a962a49fd6ca154b4be5405
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2020-09-16 17:12:12 +0600

    detecting face mask

---
+++ face.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
+model = tf.keras.models.load_model('my_model.h5')
+
+face_clsfr=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+labels_dict={0:'without_mask',1:'with_mask'}
+color_dict={0:(0,0,255),1:(0,255,0)}
+
+size = 4
+webcam = cv2.VideoCapture(0) #Use camera 0
+
+# We load the xml file
+classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    (rval, im) = webcam.read()
+    im=cv2.flip(im,1,1) #Flip to act as a mirror
+
+    # Resize the image to speed up detection
+    mini = cv2.resize(im, (im.shape[1]//size, im.shape[0]//size))
+
+    # detect MultiScale / faces 
+    faces = classifier.detectMultiScale(mini)
+
+    # Draw rectangles around each face
+    for f in faces:
+        (x, y, w, h) = [v * size for v in f] #Scale the shapesize backup
+        #Save just the rectangle faces in SubRecFaces
+        face_img = im[y:y+h, x:x+w]
+#         plt.imshow(face_img)
+        resized=cv2.resize(face_img,(150,150))
+        normalized=resized/255.0
+        reshaped=np.reshape(normalized,(1,150,150,3))
+        reshaped = np.vstack([reshaped])
+        result=model.predict(reshaped,batch_size=10)
+        print(result)
+        
+        if result[0][0]>0.5:
+            label = 1
+        else:
+            label = 0
+      
+        cv2.rectangle(im,(x,y),(x+w,y+h),color_dict[label],2)
+        cv2.rectangle(im,(x,y-40),(x+w,y),color_dict[label],-1)
+        cv2.putText(im, labels_dict[label], (x, y-10),cv2.FONT_HERSHEY_SIMPLEX,0.8,(255,255,255),2)
+        
+    # Show the image
+    cv2.imshow('Face Mask Detection',   im)
+    key = cv2.waitKey(10)
+    # if Esc key is press then break out of the loop 
+    if key == 27: #The Esc key
+        break
+# Stop video
+webcam.release()
+
+# Close all started windows
+cv2.destroyAllWindows()
